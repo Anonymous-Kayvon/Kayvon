967117cb66e77a58bdd46ba2899caaff10dadcfe
Kayvon <kayvongolf@gmail.com>
2018-03-08 15:57:49 -0800

    escape:wq
    t config --global user.name "Your Name"i# Please enter the commit message for your changes. Lines starting

---
+++ src/Math.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Math {
+
+	public static void main(String[] args) {
+		int number = 0; 
+		double asdf = 1.5;
+		if(number == 0) 
+			System.out.println ( "It's not even or odd");
+		int input = 18;	
+
+		if(input>= 15) {
+			System.out.println("ye");
+		}
+		if(number==0) {
+			System.out.println("Yousuf");
+		}
+		else if(number>0) {
+			System.out.println("Hunter");
+
+		}
+		else if(number<0) {
+			System.out.println("Hello");
+			System.out.println("Hello");
+		}
+
+
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		System.out.println(name+" y."
+		+("happy birthday");
+		
+		import java.utill.Scanner; 
+		//IF   >=0, TRUE; IF<0, FALSE 
+		public static void main(String  args) 
+		Scanner scanner = new Scanner(System.in); 
+		
+		int num = Integer.parseInt(scanner.next());//Number from user
+		if(num >= 0)
+
+		boolean rain = true; 
+
+			if(rain) {
+				//get an umbrella'
+			}
+			else { 
+				//don't '
+			}
+		Scanner scanner = new Scanner(System.in);
+		 
+	}
+}
